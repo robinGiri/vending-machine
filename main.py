256d36723c452e994555c1d14f16bb1db22b5b16
abhisekthapam <ahisekmagar250203@gmail.com>
2023-01-03 13:52:44 +0545

    seed created and looped over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from tkinter import messagebox
+import seed
+
 main = Tk()
 main.state("zoomed")
 
@@ -8,48 +9,15 @@ main.state("zoomed")
 main.title("Vending Machine")
 
 # this will create a label widget
-l1 = Label(main, text = "Drinks")
-l2 = Label(main, text = "Chocolate")
-l3 = Label(main, text = "Chips")
-l4 = Label(main, text = "Biscuits")
+space1 = Label(main).grid(row=0,column=0,padx=50,pady=50)
+space2 = Label(main).grid(row=1,column=1,padx=50,pady=50)
 
-l5 = Label(main, text = "Drinks")
-l6 = Label(main, text = "Chocolate")
-l7 = Label(main, text = "Chips")
-l8 = Label(main, text = "Biscuits")
-
-l9 = Label(main, text = "Drinks")
-l10 = Label(main, text = "Chocolate")
-l11 = Label(main, text = "Chips")
-l12 = Label(main, text = "Biscuits")
-
-l13 = Label(main, text = "Drinks")
-l14 = Label(main, text = "Chocolate")
-l15 = Label(main, text = "Chips")
-l16 = Label(main, text = "Biscuits")
-
-# grid method to arrange labels in respective
-# rows and columns as specified
-
-l1.grid(row = 0, column = 1, pady = 10, padx = 10)
-l2.grid(row = 1, column = 1, pady = 10, padx = 10)
-l3.grid(row = 2, column = 1, pady = 10, padx = 10)
-l4.grid(row = 3, column = 1, pady = 10, padx = 10)
-
-l5.grid(row = 0, column = 2, pady = 10, padx = 10)
-l6.grid(row = 1, column = 2, pady = 10, padx = 10)
-l7.grid(row = 2, column = 2, pady = 10, padx = 10)
-l8.grid(row = 3, column = 2, pady = 10, padx = 10)
-
-l9.grid(row = 0, column = 3, pady = 10, padx = 10)
-l10.grid(row = 1, column = 3, pady = 10, padx = 10)
-l11.grid(row = 2, column = 3, pady = 10, padx = 10)
-l12.grid(row = 3, column = 3, pady = 10, padx = 10)
-
-l13.grid(row = 0, column = 4, pady = 10, padx = 10)
-l14.grid(row = 1, column = 4, pady = 10, padx = 10)
-l15.grid(row = 2, column = 4, pady = 10, padx = 10)
-l16.grid(row = 3, column = 4, pady = 10, padx = 10)
+i = 0
+while i < len(seed.items):
+    Label(main, text= list(seed.items)[i]).grid(row=i*2+2,column=2,padx=5)
+    for j,options in enumerate(seed.items[list(seed.items)[i]]):
+        Label(main,text=options).grid(row=i*2+2,column=j+3,padx=5)
+    i +=1
 
 main.mainloop()
 
