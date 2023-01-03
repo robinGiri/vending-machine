91308ff59c90fb68089eb1fa4c4482ff0edc3690
Abhisek Thapa <112496493+abhisekthapam@users.noreply.github.com>
2023-01-03 13:56:22 +0545

    seed created and looped over (#1)
    Co-authored-by: abhisekthapam <ahisekmagar250203@gmail.com>

---
+++ seed.py
@@ -0,0 +1,2 @@
+items = {"Drinks":["Coke","Fanta","Slice","Sprite"],"Vegetables": ["Potato","Tomato","Onion","Pumpkin"],
+         "Fruits":["Apple","Banana","Mango","Peer"],"Meats":["Buffalo","Cow","Ox","Pig"]}
