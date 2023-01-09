1622f2debf5214ed54334d9268ea87924929a491
Anukul Nepal <anukulnepal2014@gmail.com>
2023-01-09 09:07:50 +0545

    DataSeeding

--- seed.py
+++ seed.py
@@ -1,2 +1,5 @@
-items = {"Drinks":["Coke","Fanta","Slice","Sprite"],"Vegetables": ["Potato","Tomato","Onion","Pumpkin"],
-         "Fruits":["Apple","Banana","Mango","Peer"],"Meats":["Buffalo","Cow","Ox","Pig"]}
+vending_items1= {"Drinks":["Coke","Fanta","Sprite","Slice"],"price":120,"quantity":5}
+vending_items2= {"Biscuits":["Bourbon","Oreo","Digestive","Hide&Seek"],"price":100,"quantity":5}
+vending_items3= {"Chips":["Dorritos","Lays","Local-Chips","Uncle Chips"],"price":80,"quantity":5}
+vending_items4= {"Chocolates":["Snickers","Twix","Rafaello","Mars"],"price":70,"quantity":5}
+
